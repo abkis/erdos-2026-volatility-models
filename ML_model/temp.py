75ab6a91230c0bc8162a8d79f31fac31dfa06367
abkis <kis.anna.borbala@gmail.com>
2026-06-24 14:02:45 -0400

    notes, tempfile

---
+++ ML_model/temp.py
@@ -0,0 +1,482 @@
+----- Sector: Real Estate -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.008009194346979422
+rmse = 0.08949410230277424
+mae = 0.06739777723544259
+r2 = 0.09450315456951908
+mape = 0.4473314443625885
+Figure(1200x12400)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.03679730082432159
+rmse = 0.19182622559056306
+mae = 0.1249719855470689
+r2 = -1.2799979463227067
+mape = 0.4982207122288702
+Figure(1200x12400)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.020225818134034564
+rmse = 0.1422175029102767
+mae = 0.09608884849872966
+r2 = -0.32498011314451336
+mape = 0.43617152089492633
+Figure(1200x12400)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Basic Materials -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.015929833930315974
+rmse = 0.12621344591728717
+mae = 0.0935730016538294
+r2 = 0.14786972300708046
+mape = 0.5071449032739008
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.032045735933303364
+rmse = 0.17901322837517725
+mae = 0.12849236766380714
+r2 = -0.14239088413375778
+mape = 0.4417291144009767
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.027298035412812633
+rmse = 0.16522117120034174
+mae = 0.11720225391562968
+r2 = -0.02037884029601833
+mape = 0.43739857440944285
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Consumer Defensive -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.018281660220467313
+rmse = 0.13520968981721432
+mae = 0.08674890776944914
+r2 = 0.04134665290062245
+mape = 0.5177342525589529
+Figure(1200x14800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.024177627053395302
+rmse = 0.15549156585935875
+mae = 0.09958087620722261
+r2 = -0.10219732145977756
+mape = 0.4988106553354897
+Figure(1200x14800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.018791046469950813
+rmse = 0.13708043795505911
+mae = 0.08978546636224607
+r2 = 0.008639878250248478
+mape = 0.5017589794675017
+Figure(1200x14800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Communication Services -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.029393498723140458
+rmse = 0.17144532283833366
+mae = 0.1117285568606822
+r2 = 0.15793548973514315
+mape = 0.5034443318665458
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.049478918155775514
+rmse = 0.2224385716456917
+mae = 0.13924219636786392
+r2 = 0.07669721438798494
+mape = 0.43815897323630865
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.03761112407454668
+rmse = 0.1939358761924845
+mae = 0.12017116419944533
+r2 = 0.19039205126829806
+mape = 0.4528351735116994
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Technology -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.032971036374066426
+rmse = 0.18157928398929882
+mae = 0.11248114966528239
+r2 = 0.24682804284858573
+mape = 0.5281905124049504
+Figure(1200x32800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.03337407008035732
+rmse = 0.18268571394708816
+mae = 0.12551927891239562
+r2 = 0.17148448330461064
+mape = 0.37392816026524905
+Figure(1200x32800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.030823482138611757
+rmse = 0.1755661759525785
+mae = 0.11418546721803734
+r2 = 0.27150082153141375
+mape = 0.39558025105024897
+Figure(1200x32800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Utilities -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.023585095097442506
+rmse = 0.1535743959696489
+mae = 0.06752952019996522
+r2 = 0.06781557050513909
+mape = 0.41833289677902896
+Figure(1200x12800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.021011432409589133
+rmse = 0.14495320765539868
+mae = 0.09711912048687289
+r2 = -0.35541080071161324
+mape = 0.4260449753348537
+Figure(1200x12800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.013491006215487968
+rmse = 0.11615079085175428
+mae = 0.07770848562733672
+r2 = -0.04516108193941859
+mape = 0.42255258872581203
+Figure(1200x12800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Energy -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.0181476555219994
+rmse = 0.1347132343981073
+mae = 0.1008510869724648
+r2 = 0.19020044530404345
+mape = 0.4883550352711632
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.047326760039872226
+rmse = 0.21754714440753348
+mae = 0.1578424713225606
+r2 = -0.4212048987772865
+mape = 0.4432378936757844
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.027825164471067206
+rmse = 0.16680876616972865
+mae = 0.12405959730507449
+r2 = 0.06722886991301369
+mape = 0.35975317584401395
+Figure(1200x8800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Industrials -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.018382907338528213
+rmse = 0.13558358063765766
+mae = 0.09272533323158309
+r2 = 0.16220945706720447
+mape = 0.48670988221113004
+Figure(1200x28000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.02577342358149135
+rmse = 0.160541033949241
+mae = 0.11002086649646517
+r2 = -0.014999575661813314
+mape = 0.4210772423335388
+Figure(1200x28000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.019787269708136025
+rmse = 0.14066723039903795
+mae = 0.0970290913026001
+r2 = 0.11244273280891304
+mape = 0.40831757921551404
+Figure(1200x28000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Healthcare -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+err:  index 14 is out of bounds for axis 0 with size 2
+skipping...
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+err:  negative dimensions are not allowed
+skipping...
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+err:  negative dimensions are not allowed
+skipping...
+
+
+----- Sector: Financial Services -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.010692228533687714
+rmse = 0.10340323270424244
+mae = 0.07845110307374238
+r2 = 0.13103693894652668
+mape = 0.514993668237536
+Figure(1200x26800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.019873348620228343
+rmse = 0.1409728648365647
+mae = 0.09956661529134053
+r2 = 0.020811798346252908
+mape = 0.3959187612548918
+Figure(1200x26800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.019180214455056296
+rmse = 0.13849265126733726
+mae = 0.09536042868229044
+r2 = 0.0013555881671023506
+mape = 0.39930449093107645
+Figure(1200x26800)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- Sector: Consumer Cyclical -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.025322899442024344
+rmse = 0.15913170470407317
+mae = 0.10407325992560532
+r2 = 0.1746091930895144
+mape = 3244080675324.3623
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.039268018977173184
+rmse = 0.1981615981394306
+mae = 0.136368305545434
+r2 = 0.22886857481186318
+mape = 469083248265.8632
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.03374396687390428
+rmse = 0.18369530988543034
+mae = 0.12199556502645838
+r2 = 0.23111228396835715
+mape = 616689674655.841
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+----- All Tickers -----
+
+
+ stable Time Period 2015-01-01 to 2018-12-31
+
+mse = 0.02531559101364552
+rmse = 0.15910873958914237
+mae = 0.10406334457142247
+r2 = 0.1751367419530152
+mape = 3489130452030.089
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ unstable Time Period 2019-01-01 to 2022-12-31
+
+mse = 0.039003098978166
+rmse = 0.19749202256842174
+mae = 0.13589953563239013
+r2 = 0.23111470064047435
+mape = 485926739691.2511
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+ full Time Period 2015-01-01 to 2022-12-31
+
+mse = 0.03372246478417875
+rmse = 0.18363677405187326
+mae = 0.12205548078263231
+r2 = 0.23209889704750564
+mape = 664347262090.7217
+Figure(1200x22000)
+Figure(600x600)
+Figure(800x500)
+Figure(1200x500)
+
+
+
+
+
+
+
+['CTVA', 'DOW']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['K']: possibly delisted; no timezone found
+['KVUE']: possibly delisted; no price data found  (1d 2015-01-01 -> 2022-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1672462800")
+['FOXA', 'FOX']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['IPG', 'PARA']: possibly delisted; no timezone found
+7 Failed downloads:
+['CRWD', 'PLTR', 'UBER']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['FI', 'DAY', 'ANSS', 'JNPR']: possibly delisted; no timezone found
+2 Failed downloads:
+['CEG', 'GEV']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['HES']: possibly delisted; no timezone found
+4 Failed downloads:
+['VLTO', 'AMTM', 'OTIS', 'CARR']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['SOLV', 'GEHC']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
+['WBA', 'HOLX']: possibly delisted; no timezone found
+['DFS', 'MMC']: possibly delisted; no timezone found
+1 Failed download:
+['ABNB']: possibly delisted; no price data found  (1d 2015-01-01 -> 2018-12-31) (Yahoo error = "Data doesn't exist for startDate = 1420088400, endDate = 1546232400")
